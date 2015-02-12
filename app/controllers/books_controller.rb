fa5cbba11c8e3e29323c7b7fe0cf6d7bb572625f
Jessica Meyer <JessLynnMeyer@gmail.com>
2015-02-12 16:41:30 -0600

    add sass, update controller, new images, change route

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,40 @@
 class BooksController < ApplicationController
 
-    def index
-    end
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    respond_with @book
+  end
+
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to @book
+  end
+
+  # def create
+  #  @books = JSON.parse RestClient.get('https://www.googleapis.com/books/v1/volumes?q="fiction"')
+  # end
+  
+  def update
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+  
+private
+
+  def book_params
+    params.require(:book).permit(:title, :description, :author, :imageLink)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # get 'books/show_books'
 
  root "welcome#index"
+ 
 
+ resources :books
 end
